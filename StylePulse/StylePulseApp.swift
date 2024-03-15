a0999b3d5cf0634a162e327c0fdfe8445e2509ac
kavindu-specs <kavinduprabodya@gmail.com>
2024-03-15 12:59:04 +0530

    Initial Commit

---
+++ StylePulse/StylePulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StylePulseApp.swift
+//  StylePulse
+//
+//  Created by Kavindu Prabodya on 2024-03-15.
+//
+
+import SwiftUI
+
+@main
+struct StylePulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
